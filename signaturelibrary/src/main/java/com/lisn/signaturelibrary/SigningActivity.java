fd06d8a268b74b311c78fa6f90980abe32b6159f
cnlisn <954375802@qq.com>
2017-05-03 11:47:09 +0800

    电子签名添加视频录像功能

--- signaturelibrary/src/main/java/com/lisn/signaturelibrary/SigningActivity.java
+++ signaturelibrary/src/main/java/com/lisn/signaturelibrary/SigningActivity.java
@@ -7,6 +7,8 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PaintFlagsDrawFilter;
+import android.hardware.Camera;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.Handler;
@@ -16,6 +18,8 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
@@ -25,10 +29,13 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+
+
 
 public class SigningActivity extends Activity {
 
-//    @Override
+    //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_signing);
@@ -108,6 +115,7 @@ public class SigningActivity extends Activity {
         iv.setOnTouchListener(new View.OnTouchListener() {
             int startX;
             int startY;
+
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getAction()) {
@@ -134,12 +142,180 @@ public class SigningActivity extends Activity {
                 return true;
             }
         });
+        Log.e("-----", "onCreate: " + findFrontFacingCamera());
+        if (findFrontFacingCamera() != -1) {
+            InitSurfaceView();
+            initCamera();
+
+            handler88.sendEmptyMessageDelayed(89, 1000);
+        } else {
+            Toast.makeText(this, "手机没有前置摄像头", Toast.LENGTH_SHORT).show();
+        }
+
+        Log.e("----", "onCreate: " );
+
     }
+
+    private Handler handler88 = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Record();
+        }
+    };
+///////////////////////////////////////////////////////////////////////////
+// 录像
+///////////////////////////////////////////////////////////////////////////
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        initCamera();
+        Log.e("-----", "onNewIntent: " );
+        handler88.sendEmptyMessageDelayed(88, 500);
+    }
+
+    /**
+     * 找前置摄像头,没有则返回-1
+     *
+     * @return cameraId
+     */
+    private int findFrontFacingCamera() {
+        int cameraId = -1;
+        //获取摄像头个数
+        int numberOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                cameraId = i;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
+    SurfaceHolder mSurfaceHolder;
+
+    private void InitSurfaceView() {
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.camera_show_view);
+        surfaceView.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        surfaceView.getHolder().setFixedSize(1280, 720);
+        surfaceView.getHolder().setKeepScreenOn(true);
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                mSurfaceHolder = holder;
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                mSurfaceHolder = holder;
+//                initCamera();
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                mSurfaceHolder = null;
+            }
+        });
+    }
+
+    private Camera myCamera;
+
+    //初始化Camera设置
+    public void initCamera() {
+        if (myCamera == null) {
+            try {
+                myCamera = Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT);//前置摄像头
+                Camera.Parameters myParameters = myCamera.getParameters();
+                myCamera.setParameters(myParameters);
+                myCamera.setPreviewDisplay(mSurfaceHolder);
+                myCamera.startPreview();
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.e("----", "initCamera: " + e);
+            }
+        }
+    }
+
+    private void Stop() {
+        if (myCamera != null) {
+            myCamera.stopPreview();
+            myCamera.lock();
+            myCamera.release();
+        }
+        if (mediaRecorder != null) {
+            mediaRecorder.stop();
+            mediaRecorder.release();
+            mediaRecorder = null;
+        }
+    }
+
+    private MediaRecorder mediaRecorder;
+    private String Sp_path;
+
+    private void Record() {
+        try {
+            if (myCamera==null){
+                initCamera();
+            }
+            if (mediaRecorder == null) {
+                String path = this.getFilesDir().getPath();
+                File file1 = new File(path + File.separator + "Sp");
+                if (!file1.exists()) {
+                    file1.mkdirs();
+                }
+                String child = System.currentTimeMillis() + ".mp4";
+                Sp_path = (file1.getAbsolutePath() + File.separator + child);
+                File videoFile = new File(file1, child);
+                mediaRecorder = new MediaRecorder();
+
+                myCamera.unlock();
+                mediaRecorder.setCamera(myCamera);
+                mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+                mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                mediaRecorder.setVideoSize(320, 240);
+                mediaRecorder.setVideoFrameRate(5);
+                mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+                mediaRecorder.setOutputFile(videoFile.getAbsolutePath());
+                mediaRecorder.setPreviewDisplay(mSurfaceHolder.getSurface());
+                mediaRecorder.setOrientationHint(270);
+            }
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Stop();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+// 录像
+///////////////////////////////////////////////////////////////////////////
+
     public void clear(View view) {
         canvas.drawColor(Color.WHITE);
         iv.setImageBitmap(baseBitmap);
     }
-    private int QMrequestCode=188;
+
+    private int QMrequestCode = 188;
+
     public void save(View view) {
         try {
             String path = this.getFilesDir().getPath();
@@ -149,7 +325,7 @@ public class SigningActivity extends Activity {
                 file1.mkdirs();
             }
             String child = System.currentTimeMillis() + ".jpg";
-            qm_path=(file1.getAbsolutePath()+File.separator+ child);
+            qm_path = (file1.getAbsolutePath() + File.separator + child);
             File file = new File(file1, child);
 
             FileOutputStream stream = new FileOutputStream(file);
@@ -163,9 +339,9 @@ public class SigningActivity extends Activity {
                     .getExternalStorageDirectory()));
             sendBroadcast(intent);
 
-            Intent intentd=new Intent();
+            Intent intentd = new Intent();
             intentd.putExtra("qm_path", qm_path);
-            setResult(QMrequestCode,intentd);
+            setResult(QMrequestCode, intentd);
             finish();
 
         } catch (Exception e) {
